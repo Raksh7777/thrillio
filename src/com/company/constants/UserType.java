f54d772cd963366ff0beefeacf76773c8009be25
Rakshika Bawari <rakshika7777@gmail.com>
2021-06-06 23:37:10 +0530

    Constant Exporting classes and data store

---
+++ src/com/company/constants/UserType.java
@@ -0,0 +1,8 @@
+package com.company.constants;
+
+public class UserType {
+    private UserType(){}
+    public static  final String USER="user";
+    public static  final String EDITOR="user";
+    public static  final String CHIEF_EDITOR="chiefeditor";
+}
