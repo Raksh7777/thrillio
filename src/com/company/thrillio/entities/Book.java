2b9c5a548c639a6b2a54ff07e6745607f3ba6bbb
Rakshika Bawari <rakshika7777@gmail.com>
2021-06-06 22:26:58 +0530

    Initial commit

---
+++ src/com/company/thrillio/entities/Book.java
@@ -0,0 +1,49 @@
+package com.company.thrillio.entities;
+
+public class Book extends Bookmark{
+    private int publication;
+    private String publisher;
+    private String[] authors;
+    private String genre;
+    private double amazonRating;
+
+    public int getPublication() {
+        return publication;
+    }
+
+    public void setPublication(int publication) {
+        this.publication = publication;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String[] getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String[] authors) {
+        this.authors = authors;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public double getAmazonRating() {
+        return amazonRating;
+    }
+
+    public void setAmazonRating(double amazonRating) {
+        this.amazonRating = amazonRating;
+    }
+}
