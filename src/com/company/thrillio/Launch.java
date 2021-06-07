58e97e38606d63de06ca160e01a1e5b2807add21
Rakshika Bawari <rakshika7777@gmail.com>
2021-06-07 15:15:44 +0530

    Creating Dao Classes

---
+++ src/com/company/thrillio/Launch.java
@@ -0,0 +1,42 @@
+package com.company.thrillio;
+
+import com.company.managers.BookmarkManager;
+import com.company.managers.UserManager;
+import com.company.thrillio.entities.Bookmark;
+import com.company.thrillio.entities.User;
+
+import static com.company.thrillio.DataStore.loadData;
+
+public class Launch {
+
+    private static User[] users;
+    private static Bookmark[][] bookmarks;
+    private static void loadData(){
+        System.out.println("1. Loading data...");
+        DataStore.loadData();
+        users= UserManager.getInstance().getUsers();
+        bookmarks= BookmarkManager.getInstance().getBookmarks();
+        System.out.println("Printing data...");
+        printUserData();
+        printBookmarkData();
+    }
+
+    private static void printBookmarkData() {
+        for(Bookmark [] bookmarkList:bookmarks){
+            for (Bookmark bookmark:bookmarkList){
+               System.out.println(bookmark);
+            }
+
+            }
+        }
+
+
+    private static void printUserData(){
+        for(User user :users){
+            System.out.println(user);
+        }
+    }
+    public static void main(String[] args){
+        loadData();
+    }
+}
