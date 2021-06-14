e2157f8e959cab2d5b1794a95c810ba1b4fd739e
Rakshika Bawari <rakshika7777@gmail.com>
2021-06-14 22:40:07 +0530

    project extension

--- src/com/company/thrillio/entities/WebLink.java
+++ src/com/company/thrillio/entities/WebLink.java
@@ -1,9 +1,18 @@
 package com.company.thrillio.entities;
 
-public class WebLink extends Bookmark{
+import com.company.thrillio.partner.Shareable;
+
+public class WebLink extends Bookmark implements Shareable {
     private String url;
     private String host;
-
+    private String htmlPage;
+    private DownloadStatus downloadStatus = DownloadStatus.NOT_ATTEMPTED;
+    public enum DownloadStatus {
+        NOT_ATTEMPTED,
+        SUCCESS,
+        FAILED,
+        NOT_ELIGIBLE; // not eligible for download
+    }
     @Override
     public String toString() {
         return "WebLink{" +
@@ -27,4 +36,34 @@ public class WebLink extends Bookmark{
     public void setHost(String host) {
         this.host = host;
     }
+    @Override
+    public String getItemData() {
+        // TODO Auto-generated method stub
+        StringBuilder builder = new StringBuilder();
+        builder.append("<item>");
+        builder.append("<type>WebLink</type>");
+        builder.append("<title>").append(getTitle()).append("</title>");
+        builder.append("<url>").append(url).append("</url>");
+        builder.append("<host>").append(host).append("</host>");
+        builder.append("</item>");
+        return builder.toString();
+    }
+
+    public DownloadStatus getDownloadStatus() {
+        return downloadStatus;
+    }
+
+    public void setDownloadStatus(DownloadStatus downloadStatus) {
+        this.downloadStatus = downloadStatus;
+    }
+
+    public String getHtmlPage() {
+        return htmlPage;
+    }
+
+    public void setHtmlPage(String htmlPage) {
+        this.htmlPage = htmlPage;
+    }
+
 }
+
